ea0576b2838e947d6190894133da1c89926dbf14
Pahlorus <katyshev1980@yandex.ru>
2018-09-04 15:05:06 +0200

    Create scripts, correct unity project 04.09.18

---
+++ Sapper_unity/Sapper_unity/Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    #region Fields
+
+    #endregion
+
+    #region Fields Initialized in Unity
+    [SerializeField]
+
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Unity Metods
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region Metods
+
+    #endregion
+
+}
---
+++ Sapper_unity/Sapper_unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    #region Fields
+    private Sprite _underTexture;
+    private int _numberBomb;
+    private bool _isBomb;
+    #endregion
+
+    #region Fields Initialized in Unity
+    [SerializeField]
+    #endregion
+
+    public Tile(bool isBomb )
+    {
+        _isBomb = isBomb;
+    }
+
+    #region Unity Metods
+    private void Awake()
+    {
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Metods
+
+    #endregion
+}
